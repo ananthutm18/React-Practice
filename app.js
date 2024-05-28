84c0b728eca87f4a915f462b7fba0080bcdc00f6
Ananthu TM(ATM) <ananthutm18@gmail.com>
2024-05-28 16:06:06 +0530

    second commit jus some random workouts

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const head=React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},
-    [React.createElement("h1",{id:"head"},"hello  world"),
-    React.createElement("h1",{id:"heading"},"IAm H2 head")]
-),React.createElement("div",{id:"child"},
-[React.createElement("h1",{id:"head"},"hello  world"),
-React.createElement("h1",{id:"heading"},"IAm H2 head")]
-)])
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(head)
+import React from "react";
+import ReactDOM from "react-dom";
+const js=<h1>Hii iiama a peace of jsx</h1>
+const Ele=()=>{
+    return <h1>Hello Iam a functional component</h1>
+}
+const Div=()=>(
+
+    <div>{Ele()} {js}</div>
+   
+)
+    
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Div/>);
